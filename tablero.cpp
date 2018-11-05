dcb17f82b1d829a41a74e5f89eb90179cae835d8
Rene Melendez <rene.melendeza@gmail.com>
2018-11-05 14:30:04 -0600

    Nuevo archivo tablero, listo para usar

--- tablero.cpp
+++ tablero.cpp
@@ -1,405 +1,112 @@
+#include <cstdlib>
 #include <iostream>
-#include <stdlib.h>
-#include <cmath>
 
 using namespace std;
 
-struct Casilla			//Casilla norte
+struct Nodo {
+	// Cuarto punteros: hacia upera, hacia downntro,
+	// hacia el siguiente y hacia el anterior.
+	Nodo *up, *down, *sig, *ant;
+	// Otros campos, podr�an ser, por ejemplo: campo
+	// para la ficha y coordenadas de cada nodo.
+};
+
+// Punteros auxiliares para crear cada anillo del tablero.
+// Luego se har� que pInicio apunte a "toda" la estructura.
+Nodo *pAnillo1 = NULL;
+Nodo *pAnillo2 = NULL;
+Nodo *pAnillo3 = NULL;
+Nodo *pAnillo4 = NULL;
+Nodo *pAnillo5 = NULL;
+
+class Tablero {
+private:
+	Nodo *pInicio;
+public:
+	Tablero();
+	void crearAnillo(Nodo **, int);
+	void verificar();
+};
+
+Tablero::Tablero()
 {
-	int color; 			//COLOR 0 ES IGUAL A NO COLOR  COLOR 1 ES IGUAL A ROJO  COLOR 2 IGUAL AZUL
-	Casilla*sig;
-	Casilla*ant;
-	Casilla*up;
-	Casilla*down;
-}*lsinicio, *lsfinal, 
-*NorteA, *OesteA, *SurA, *EsteA,
-*NorteB, *OesteB, *SurB, *EsteB,
-*NorteC, *OesteC, *SurC, *EsteC,
-*NorteD, *OesteD, *SurD, *EsteD,
-*NorteE, *OesteE, *SurE, *EsteE;
-
- 
-
-void crearTablero(){
-	//CREANDO ANILLOS DEL UNO AL 3 CON 16 CASILLAS 
-	for (int k=0; k<3; k++){
-		for (int i=0; i<16; i++){
-			Casilla*novo= new Casilla;
-			if (lsinicio==NULL){
-				lsinicio=novo;
-				lsinicio->sig=lsinicio;
-				lsinicio->ant=lsinicio;
-				lsfinal= lsinicio;
-				lsinicio->color=0;
-				if (k==0){
-					NorteA=novo;
-				}
-				else if (k==1){
-					NorteB=novo;
-				}
-				else if (k==2){
-					NorteC=novo;
-				}
-			}
-			else {
-				lsfinal->sig=novo;
-				novo->sig=lsinicio;
-				novo->ant=lsfinal;
-				lsfinal=novo;
-				lsinicio->ant=lsfinal;
-				if (i==4 and k==0){
-					OesteA=novo;
-					OesteA->color=0;
-				}
-				else if (i==8 and k==0){
-					SurA=novo;
-					SurA->color=0;
-				}
-				else if (i==12 and k==0){
-					EsteA=novo;
-					EsteA->color=0;
-				}
-				else if (i==4 and k==1){
-					OesteB=novo;
-					OesteB->color=0;
-				}
-				else if (i==8 and k==1){
-					SurB=novo;
-					SurB->color=0;
-				}
-				else if (i==12 and k==1){
-					EsteB=novo;
-					EsteB->color=0;
-				}
-				else if (i==4 and k==2){
-					OesteC=novo;
-					OesteC->color=0;
-				}
-				else if (i==8 and k==2){
-					SurC=novo;
-					SurC->color=0;
-				}
-				else if (i==12 and k==2){
-					EsteC=novo;
-					EsteC->color=0;
-				}
-			}
-		}
-		lsinicio=NULL;
+	pInicio = NULL;
+	// Primero se crean los cinco anillos
+	// doblemente enlazados:
+	crearAnillo(&pAnillo1, 16);
+	crearAnillo(&pAnillo2, 16);
+	crearAnillo(&pAnillo3, 16);
+	crearAnillo(&pAnillo4, 8);
+	crearAnillo(&pAnillo5, 4);
+	// Luego se unen los anillos entre s�, con
+	// dobles enlaces, de acuerdo a como se unen
+	// los c�rculos en el tablero del juego:
+	Nodo *s1 = pAnillo1;
+	Nodo *s2 = pAnillo2;
+	// Este lazo une el anillo m�s externo con el segundo
+	// por medio de 4 enlaces:
+	for (int i = 1; i <= 4; i++) {
+		s1->down = s2;
+		s2->up = s1;
+		s1 = s1->sig->sig->sig->sig;
+		s2 = s2->sig->sig->sig->sig;
 	}
-	lsinicio=NULL;
-
-	for (int i=0; i<8;i++){
-		Casilla*novo= new Casilla;
-		if (lsinicio==NULL){
-				lsinicio=novo;
-				lsinicio->sig=lsinicio;
-				lsinicio->ant=lsinicio;
-				lsfinal=lsinicio;
-				NorteD=novo;
-				
-			}
-		else {
-			lsfinal->sig=novo;
-			novo->sig=lsinicio;
-			novo->ant=lsfinal;
-			lsfinal=novo;
-			lsinicio->ant=lsfinal;
-
-			if (i==2){
-				OesteD=novo;
-				OesteD->color=0;
-			}
-			else if (i==4){
-				SurD=novo;
-				SurD->color=0;
-			}
-			else if (i==6){
-				EsteD=novo;
-				EsteD->color=0;
-			}
-		}
-
+	// Este lazo une el segundo anillo con el tercero
+	// por medio de 16 enlaces:
+	s2 = pAnillo2;
+	Nodo *s3 = pAnillo3;
+	for (int i = 1; i <= 16; i++) {
+		s2->down = s3;
+		s3->up = s2;
+		s2 = s2->sig;
+		s3 = s2->sig;
 	}
-	lsinicio=NULL;
-	//Creando anillo ultimo E
-
-	for (int i=0;i<4;i++){
-		Casilla*novo= new Casilla;
-		if (lsinicio==NULL){
-				lsinicio=novo;
-				lsinicio->sig=lsinicio;
-				lsinicio->ant=lsinicio;
-				lsfinal=lsinicio;
-				NorteE=novo;
-				
-			}
-		else {
-			lsfinal->sig=novo;
-			novo->sig=lsinicio;
-			novo->ant=lsfinal;
-			lsfinal=novo;
-			lsinicio->ant=lsfinal;
-
-			if (i==1){
-				OesteE=novo;
-				OesteE->color=0;
-			}
-			else if (i==2){
-				SurE=novo;
-				SurE->color=0;
-			}
-			else if (i==3){
-				EsteE=novo;
-				EsteE->color=0;
-			}
-		}
+	// Este lazo une el tercer anillo con el cuarto
+	// por medio de 8 enlaces:
+	s3 = pAnillo3;
+	Nodo *s4 = pAnillo4;
+	for (int i = 1; i <= 8; i++) {
+		s3->down = s4;
+		s4->up = s3;
+		s3 = s3->sig->sig;
+		s4 = s4->sig;
 	}
+	
+	// Conecciones anillo5 con anillo 4
+	s4=pAnillo4;
+	Nodo*s5=pAnillo5;
+	for (int i=1; i<=4;i++){
+		s5->up=s4;
+		s4->down=s5;
+		s4= s4->sig->sig;
+		s5= s5->sig;
+	}
+
+	// Finalmente se hace que pInicio sea el
+	// puntero base de la estructura ya armada:
+	pInicio = pAnillo1;
 }
 
-void conectar(){
-	//UNIENDO LINEA NORTE **VER IMAGEN DEL ESQUEMA 
-	NorteA->up=NULL;
-	NorteA->down=NorteB;
-	NorteB->up=NorteA;
-	NorteB->down=NorteC;
-	NorteC->up= NorteB;
-	NorteC->down=NorteD;
-	NorteD->up=NorteC;
-	NorteD->down=NorteE;
-	NorteE->up=NorteD;
-
-	//UNIENDO PARTE SUR
-	NorteE->down=SurE;
-	SurE->up=NorteE;
-	SurE->down=SurD;
-	SurD->up= SurE;
-	SurD->down=SurC;
-	SurC->up=SurD;
-	SurC->down=SurB;
-	SurB->up=SurC;
-	SurB->down=SurA;
-	SurA->up=SurB;
-	SurA->down=NULL;
-
-	//Uniendo parte ESTE
-	EsteA->up=NULL;
-	EsteA->down=EsteB;
-	EsteB->up=EsteA;
-	EsteB->down=EsteC;
-	EsteC->up=EsteB;
-	EsteC->down=EsteD;
-	EsteD->up= EsteC;
-	EsteD->down=EsteE;
-	EsteE->up=EsteD;
-	// pARTE OESTE
-	EsteE->down=OesteE;
-	OesteE->up= EsteE;
-	OesteE->down= OesteD;
-	OesteD->up=	OesteE;
-	OesteD->down=OesteC;
-	OesteC->up=OesteD;
-	OesteC->down=OesteB;
-	OesteB->up= OesteC;
-	OesteB->down= OesteA;
-	OesteA->up=OesteB;
-	OesteA->down=NULL;
-
-	// CONECIONES VERGONAS DE LOS DEMAS PUNTOS
-	(NorteC->sig)->up=(NorteB->sig);
-	(NorteB->sig)->down=(NorteC->sig);
-
-	((NorteC->sig)->sig)->up=((NorteB->sig)->sig);
-	((NorteB->sig)->sig)->down=((NorteC->sig)->sig);
-
-	(((NorteC->sig)->sig)->sig)->up=(((NorteB->sig)->sig)->sig);
-	(((NorteB->sig)->sig)->sig)->down=(((NorteC->sig)->sig)->sig);
-
-//CONECCIONES COLOR AZUL OESTE
-	(OesteC->sig)->up=(OesteB->sig);
-	(OesteB->sig)->down=(OesteC->sig);
-
-	((OesteC->sig)->sig)->up=((OesteB->sig)->sig);
-	((OesteB->sig)->sig)->down=((OesteC->sig)->sig);
-
-	(((OesteC->sig)->sig)->sig)->up=(((OesteB->sig)->sig)->sig);
-	(((OesteB->sig)->sig)->sig)->down=(((OesteC->sig)->sig)->sig);
-
-//CONECCIONES COLOR ROJO  SUR
-	(SurC->sig)->up=(SurB->sig);
-	(SurB->sig)->down=(SurC->sig);
-
-	((SurC->sig)->sig)->up=((SurB->sig)->sig);
-	((SurB->sig)->sig)->down=((SurC->sig)->sig);
-
-	(((SurC->sig)->sig)->sig)->up=(((SurB->sig)->sig)->sig);
-	(((SurB->sig)->sig)->sig)->down=(((SurC->sig)->sig)->sig);
-//CONECCIONES COLOR AZUL ESTE
-
-	(EsteC->sig)->up=(EsteB->sig);
-	(EsteB->sig)->down=(EsteC->sig);
-
-	((EsteC->sig)->sig)->up=((EsteB->sig)->sig);
-	((EsteB->sig)->sig)->down=((EsteC->sig)->sig);
-
-	(((EsteC->sig)->sig)->sig)->up=(((EsteB->sig)->sig)->sig);
-	(((EsteB->sig)->sig)->sig)->down=(((EsteC->sig)->sig)->sig);
-
-	//CONECCIONES CON EL ANILLO 5
-
-	(NorteD->sig)->up=((NorteC->sig)->sig);
-	((NorteC->sig)->sig)->down=(NorteD->sig);
-
-	(OesteD->sig)->up=((OesteC->sig)->sig);
-	((OesteC->sig)->sig)->down=(OesteD->sig);
-
-	(SurD->sig)->up=((SurC->sig)->sig);
-	((SurC->sig)->sig)->down=(SurD->sig);
-
-	(EsteD->sig)->up=((EsteC->sig)->sig);
-	((EsteC->sig)->sig)->down=(EsteD->sig);
-}
-
-
-void ver_conecciones(){
-	Casilla*aux=new Casilla;
-	aux= NorteA;
-	int i=100;
-	cout<<"Dato:  ANILLO 1:"<<endl;
-
-	do{
-		aux->color=i;
-		i++;
-		cout<<aux->color<<endl;
-		aux=aux->sig;
-
-	}while (aux!=NorteA);
-
-	aux= NorteB;
-	i=200;
-	cout<<"Dato:  ANILLO 2:"<<endl;
-
-	do{
-		aux->color=i;
-		i++;
-		cout<<aux->color<<endl;
-		aux=aux->sig;
-
-	}while (aux!=NorteB);
-
-	aux= NorteC;
-	i=300;
-	cout<<"Dato:  ANILLO 3:"<<endl;
-	do{
-		aux->color=i;
-		i++;
-		cout<<aux->color<<endl;
-		aux=aux->sig;
-
-	}while (aux!=NorteC);
-
-	aux= NorteD;
-	i=400;
-
-	cout<<"Dato:  ANILLO 4:"<<endl;
-
-	do{
-		aux->color=i;
-		i++;
-		cout<<aux->color<<endl;
-		aux=aux->sig;
-
-	}while (aux!=NorteD);
-
-	aux= NorteE;
-	i=500;
-	cout<<"Dato:  ANILLO 5:"<<endl;
-
-	do{
-		aux->color=i;
-		i++;
-		cout<<aux->color<<endl;
-		aux=aux->sig;
-
-	}while (aux!=NorteE);
-
-}
-
-void con_entreanillos(){
-	cout<<"COMPROBANDO CONECCIONES DE CASILLAS!!!!!!!!!!"<<endl;
-	Casilla*aux=new Casilla;
-	aux=NorteA;
-	do{
-		cout<<"Nodo: "<<aux->color<<endl;
-		if((aux->up)!=NULL){
-			cout<<"TAmbien esta conectada hacia arriba: "<<(aux->up)->color<<endl;
-		}
-		if (aux->down!=NULL){
-				cout<<" Conectado hacia abajo con:  "<<(aux->down)->color<<endl;
-			}
-		aux=aux->sig;
-
-	}while (aux!=NorteA);
-	aux=NorteB;
-	do{
-		cout<<"Nodo: "<<aux->color<<endl;
-		if((aux->up)!=NULL){
-			cout<<"TAmbien esta conectada hacia arriba: "<<(aux->up)->color<<endl;
-		}
-		if (aux->down!=NULL){
-				cout<<" Conectado hacia abajo con:  "<<(aux->down)->color<<endl;
-			}
-		aux=aux->sig;
-
-	}while (aux!=NorteB);
-
-aux=NorteC;
-	do{
-		cout<<"Nodo: "<<aux->color<<endl;
-		if((aux->up)!=NULL){
-			cout<<"TAmbien esta conectada hacia arriba: "<<(aux->up)->color<<endl;
-		}
-		if (aux->down!=NULL){
-				cout<<" Conectado hacia abajo con:  "<<(aux->down)->color<<endl;
-			}
-		aux=aux->sig;
-
-	}while (aux!=NorteC);
-
-aux=NorteD;
-	do{
-		cout<<"Nodo: "<<aux->color<<endl;
-		if((aux->up)!=NULL){
-			cout<<"TAmbien esta conectada hacia arriba: "<<(aux->up)->color<<endl;
-		}
-		if (aux->down!=NULL){
-				cout<<" Conectado hacia abajo con:  "<<(aux->down)->color<<endl;
-			}
-		aux=aux->sig;
-
-	}while (aux!=NorteD);
-
-aux=NorteE;
-	do{
-		cout<<"Nodo: "<<aux->color<<endl;
-		if((aux->up)!=NULL){
-			cout<<"TAmbien esta conectada hacia arriba: "<<(aux->up)->color<<endl;
-		}
-		if (aux->down!=NULL){
-				cout<<" Conectado hacia abajo con:  "<<(aux->down)->color<<endl;
-			}
-		aux=aux->sig;
-
-	}while (aux!=NorteE);
-
-
-}
-
-int main(int argc, char const *argv[])
+void Tablero::crearAnillo(Nodo **p, int numNodos)
 {
-	crearTablero();
-	conectar();
-	ver_conecciones();
-	con_entreanillos();
+	int i;
+	*p = new Nodo;
+	Nodo *nuevo;
+	Nodo *s = *p;
+
+	for (i = 2; i <= numNodos; i++) {
+		nuevo = new Nodo;
+		s->sig = nuevo;
+		s->sig->ant = s;
+		s = s->sig;
+	}
+	s->sig = *p;
+	s->sig->ant = s;
+}
+
+int main(void)
+{
+	Tablero tableroJuego;
+
 	return 0;
 }
